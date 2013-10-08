d9c965d6ede085cf417f71b40700fe3069d6dc1b
Daniel Berger <djberg96@gmail.com>
2013-10-07 23:37:16 -0600

    Pass all structs by_ref by default.

--- lib/solaris/kstat/functions.rb
+++ lib/solaris/kstat/functions.rb
@@ -7,10 +7,10 @@ module Solaris
 
     include Solaris::Structs
 
-    attach_function :kstat_chain_update, [KstatCtl], :int
-    attach_function :kstat_close, [KstatCtl], :int
-    attach_function :kstat_lookup, [KstatCtl, :string, :int, :string], KstatStruct
-    attach_function :kstat_open, [], KstatCtl
-    attach_function :kstat_read, [KstatCtl, KstatStruct, :pointer], :int
+    attach_function :kstat_chain_update, [KstatCtl.by_ref], :int
+    attach_function :kstat_close, [KstatCtl.by_ref], :int
+    attach_function :kstat_lookup, [KstatCtl.by_ref, :string, :int, :string], KstatStruct.by_ref
+    attach_function :kstat_open, [], KstatCtl.by_ref
+    attach_function :kstat_read, [KstatCtl.by_ref, KstatStruct.by_ref, :pointer], :int
   end
 end
