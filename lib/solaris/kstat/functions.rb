c008ac0a03f5c165879c47fd244bf58e1a56f5ee
Daniel Berger <djberg96@gmail.com>
2013-10-10 13:20:23 -0600

    Removed the release interface code.

--- lib/solaris/kstat/functions.rb
+++ lib/solaris/kstat/functions.rb
@@ -7,16 +7,10 @@ module Solaris
 
     include Solaris::Structs
 
-    class Solaris::Structs::KstatCtl < FFI::Struct
-      def self.release(pointer)
-        Solaris::Functions.kstat_close(pointer)
-      end
-    end
-
-    attach_function :kstat_chain_update, [KstatCtl.by_ref], :int
+    attach_function :kstat_chain_update, [KstatCtl], :int
     attach_function :kstat_close, [KstatCtl], :int
-    attach_function :kstat_lookup, [KstatCtl.by_ref, :string, :int, :string], KstatStruct.by_ref
-    attach_function :kstat_open, [], KstatCtl.auto_ptr
-    attach_function :kstat_read, [KstatCtl.by_ref, KstatStruct.by_ref, :pointer], :int
+    attach_function :kstat_lookup, [KstatCtl, :string, :int, :string], KstatStruct.by_ref
+    attach_function :kstat_open, [], KstatCtl.by_ref
+    attach_function :kstat_read, [KstatCtl, KstatStruct, :pointer], :int
   end
 end
